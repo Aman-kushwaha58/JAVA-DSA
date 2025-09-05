36815e05b6ef1c9108b215867f778a44c78fbdae
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-09-05 15:05:19 +0530

    Add files via upload

---
+++ functionfactorial.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class functionfactorial {
+    public static void printfactorial(int n) {
+        if(n<0){
+            System.out.println("invalid Number");
+            return;
+        }
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial *= i;
+        }
+        System.out.println(factorial);
+        return;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        printfactorial(n);
+       
+    }
+
+    
+}
