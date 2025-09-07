665da45e406344a061d8b5cdcc626c9cff2d0ec9
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-09-07 14:41:10 +0530

    Add files via upload

---
+++ marksarry.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class marksarry {
+    public static void main(String[] args) {
+        int marks[] = new int[3];
+        marks[0] = 98;
+        marks[1] = 95;
+        marks[2] = 92;
+       System.out.println(marks[0]);
+       System.out.println(marks[1]);
+       System.out.println(marks[2]);
+
+    }
+
+    
+}
