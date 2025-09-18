68c667952486f5d353b7b962ff81a9906c82099c
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-09-19 00:21:27 +0530

    Add files via upload

---
+++ whileloop.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class whileloop{
+    public static void main(String[] args) {
+        // int i = 1;
+        // while(i<=10){
+        //     System.out.println(i);
+        //     i++;
+        // Scanner sc = new Scanner(System.in);
+        // int n = sc.nextInt();
+        // int i = 1;
+        // while(i<=n){
+        //     System.out.println(i);
+        //     i+=2;
+        /*this for while only */
+    
+        // }
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        do{
+            System.out.println("i");
+            i++;
+        }
+        while(i<=n);
+        
+    }
+
+}
