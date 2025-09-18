4e4d1bb14fe2dd5e8e4a25b46fc9720f026bfb27
Aman-kushwaha58 <amankushwaha3328@gmail.com>
2025-09-18 09:28:18 +0530

    Add files via upload

---
+++ addtwonumberwithout.java
@@ -0,0 +1,21 @@
+public class addtwonumberwithout {
+    public static void main(String[] args) {
+        // int a = 10;
+        // int b = 20;
+        // while (b != 0) {
+        //     int carry = a & b;
+        //     a = a ^ b;
+        //     b = carry << 1;
+        // }
+        int a = 100;
+        int b = 120;
+        // System.out.println(a>b);
+        System.out.println(a==b && a<=b);
+        System.out.println(a==b || a<b);
+        System.out.println(a==b ^ a<b);
+        System.out.println(!(a==b && a<=b));
+        
+    }
+
+    
+}
